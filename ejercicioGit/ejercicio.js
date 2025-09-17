3d0e325f02976d79e81fe97b330309523326580d
MaycolSac2000 <ixinversorsmaycol@gmail.com>
2025-09-17 17:29:04 -0500

    El boton de la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,7 @@
 saludar = function (){
-    console.log("ingresa a saludar");
+   
+    let cmpImagenSaludo;
+    cmpImagenSaludo = document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/ganar_krusty.gif";
+     console.log("ingresa a saludar");
 }
