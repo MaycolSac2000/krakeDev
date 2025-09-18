b627d292e99ad4b2a801f39ff2eb57b12e31b9d0
MaycolSac2000 <ixinversorsmaycol@gmail.com>
2025-09-18 18:01:41 -0500

    Calcular el valor total y mostrarlo

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -78,6 +78,9 @@ calcularValorTotal = function () {
             mostrarTexto("lblValorIVA",valorIVA);
        
     //10. Invocar a calcularTotal y lo que devuelve guardar en la variable valorTotal
+
+           valorTotal= calcularTotal(valorSubtotal,valorDescuento,valorIVA);
+
     //11. Mostrar el resultado en el componente lblTotal
     /*
         Caso de prueba: 
@@ -93,7 +96,8 @@ calcularValorTotal = function () {
 
                 Si el caso de prueba es exitoso, hacer un commit
        */
-            
+            mostrarTexto("lblTotal",valorTotal);
+
     //12. Mostrar un resumen en el componente lblResumen, si no existe debe agregarlo
     /*
         Ejemplo: 
