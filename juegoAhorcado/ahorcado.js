3c6f5cdc3a44489a2336f646f4ff061b1a175f06
MaycolSac2000 <ixinversorsmaycol@gmail.com>
2025-09-27 20:14:20 -0500

    Paso 1

--- juegoAhorcado/ahorcado.js
+++ juegoAhorcado/ahorcado.js
@@ -1,5 +1,7 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
 
+let palabraSecreta;
+
 esMayuscula= function (caracter){
     
    let codigoLetra = caracter.charCodeAt(0);
@@ -14,3 +16,29 @@ esMayuscula= function (caracter){
 
 }
 
+guardarPalabra = function (){
+    let palabra = recuperarTexto("txtSecreta");
+    let hayMayusculas=0;
+    let longitud = palabra.length;
+    if(longitud == 5){
+        for(let i=0;i<longitud;i++){
+            let letra = palabra.charAt(i);
+            if(esMayuscula(letra)){
+                hayMayusculas = hayMayusculas+1;
+            }
+        }
+
+        if(hayMayusculas == 5){
+            palabraSecreta = palabra;
+            console.log(palabraSecreta);
+        }else{
+            alert("La palabra debe estar en letras mayusculas");
+        }
+
+    }else{
+        alert("La palabra debe ser de 5 letras");
+    }
+
+    
+
+};
