d40fee411179917a3ec4ca59127d101cdba8be6b
MaycolSac2000 <ixinversorsmaycol@gmail.com>
2025-09-27 19:46:12 -0500

    Paso 0

--- juegoAhorcado/ahorcado.js
+++ juegoAhorcado/ahorcado.js
@@ -1,1 +1,16 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+
+esMayuscula= function (caracter){
+    
+   let codigoLetra = caracter.charCodeAt(0);
+
+   if(codigoLetra>= 65 && codigoLetra<=90){
+
+    return true;
+
+   }else {
+    return false;
+   }
+
+}
+
