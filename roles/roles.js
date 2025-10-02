551747ecd6b4a2d332c42c6b26fc0f5e82c0b168
MaycolSac2000 <ixinversorsmaycol@gmail.com>
2025-10-02 17:40:03 -0500

    Se agrega la funcionon ejecutarNuevo

--- roles/roles.js
+++ roles/roles.js
@@ -4,11 +4,18 @@ let empleados = [
     { cedula: "1751219948", nombre: "Mikael", apellido: "Altamirano", sueldo: 1000.0 }
 ]
 
+let esNuevo = false
+
 mostrarOpcionEmpleado = function () {
 
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+    ocultarComponente("txtCedula");
+    ocultarComponente("txtNombre");
+    ocultarComponente("txtApellido");
+    ocultarComponente("txtSueldo");
+    ocultarComponente("btnGuardar");
     mostrarEmpleados();
 }
 
@@ -31,16 +38,16 @@ mostrarEmpleados = function () {
     let cmpTablaEmpleados = document.getElementById("tablaEmpleados");
     let generandoTabla = "<table class='miDisenoTabla'><tr><th>Cedula</th><th>Nombre</th><th>Apellido</th><th>Sueldo</th></tr>";
     let empledoRecuperado;
-    for(let i=0;i<empleados.length; i++){
+    for (let i = 0; i < empleados.length; i++) {
         empledoRecuperado = empleados[i];
 
-        generandoTabla += "<tr><td>"+empledoRecuperado.cedula+"</td>"
-        +"<td>"+empledoRecuperado.nombre+"</td>"
-        +"<td>"+empledoRecuperado.apellido+"</td>"
-        +"<td>"+empledoRecuperado.sueldo+"</td>"
-        +"</tr>";
+        generandoTabla += "<tr><td>" + empledoRecuperado.cedula + "</td>"
+            + "<td>" + empledoRecuperado.nombre + "</td>"
+            + "<td>" + empledoRecuperado.apellido + "</td>"
+            + "<td>" + empledoRecuperado.sueldo + "</td>"
+            + "</tr>";
     }
-    
+
     generandoTabla += "</table>"
     cmpTablaEmpleados.innerHTML = generandoTabla;
 
@@ -48,4 +55,11 @@ mostrarEmpleados = function () {
 
 }
 
-
+ejecutarNuevo = function () {
+    mostrarComponente("txtCedula");
+    mostrarComponente("txtNombre");
+    mostrarComponente("txtApellido");
+    mostrarComponente("txtSueldo");
+    mostrarComponente("btnGuardar"); 
+    esNuevo = true;
+}
